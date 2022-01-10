077bd826094aeaed0fdbce8820590e0be7a58a8f
CGAnVil <hoangminh.tlu@gmail.com>
2022-01-10 10:16:14 +0700

    upload bai tap

---
+++ src/com/codegym/TestCircle.java
@@ -0,0 +1,9 @@
+package com.codegym;
+
+public class TestCircle {
+    public static void main(String[] args) {
+        Circle c1 = new Circle(10);
+        System.out.println(c1.getRadius());
+        System.out.println(c1.getArea());
+    }
+}
